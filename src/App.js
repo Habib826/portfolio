b8d87317bfcf18e6df946ed2076dc84a5da9270e
Habib826 <habib826826@.com>
2021-05-02 20:56:22 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home/Home';
-import About from "./Components/Home/About/About";
+
 
 function App() {
   return (
